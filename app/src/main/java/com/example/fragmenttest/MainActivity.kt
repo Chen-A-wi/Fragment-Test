ad3b4e8acbb23aa6a2525de0f0546e88b1a6ffb3
Ben <benchen@starlux-airlines.com>
2020-01-30 15:25:07 +0800

    [feat] add fragment utils
    * 加入fragment相關工具。

--- app/src/main/java/com/example/fragmenttest/MainActivity.kt
+++ app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.fragmenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragmenttest.utils.replaceFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonEvent()
+    }
+
+    private fun buttonEvent() {
+        button.setOnClickListener {
+            replaceFragment(HelloFragment(), R.id.mainFrame)
+        }
     }
 }
